b4bfbf02f140ba1661245ffc11d812216b19a425
Pratham2415 <84369631+Pratham2415@users.noreply.github.com>
2021-07-31 11:07:47 +0530

    Add files via upload

---
+++ Student_Activity_Template.py
@@ -0,0 +1,46 @@
+import random
+import turtle
+
+turtle.speed(0)
+
+turtle.Screen().bgcolor("black")
+
+
+
+
+
+def firework1():
+    for num in range():
+        turtle.forward()
+        turtle.right(-())
+firework1()
+
+def move():
+    turtle.penup()
+    
+    
+    turtle.goto(x,y)
+    turtle.pendown()
+
+move()
+pen('yellow')
+firework1(30)
+move()
+pen('blue')
+firework1(57)
+move()
+pen('purple')
+firework1(80)
+move()
+pen('lightblue')
+firework1(120)
+
+
+
+
+
+
+
+
+
+
